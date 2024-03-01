c6dcc9152cf9f381d77c94bc03db477da5668fc2
sangT220222 <magnas9@hotmail.co.uk>
2024-03-01 22:36:30 +0000

    check_winner logic added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const game_board = (() => {
   let game_board = [
-    ['1','2','3'],
-    ['4','5','6'],
-    ['7','8','9']
+    ['','',''],
+    ['','',''],
+    ['','','']
   ];
 
   const rows = 3;
@@ -16,23 +16,35 @@ const game_board = (() => {
   //   }
   // }
 
-
-  return game_board;
+  const get_game_board = () => game_board; //getting game board
+  return game_board[0][2];
 })
 
-// const Player = ((name) => {
-//   const player = name;
-  
-//   let token = 0;
-//   if (name = "Player1"){
-//     token = 1;
-//   }
-//   if (name = "Player2"){
-//     token = 2;
-//   }
-//   return player;
-// })
 
+const check_winner = () =>{
+  for(let row = 0; row < 3 ; row ++){
+    if(game_board[row][0] === symbol && game_board[row][1] === symbol && game_board[row][2] === symbol){
+      return true //this if loop checks if the symbol is placed 3 times in a row horizontally, the it increments hence checks all the rows
+    }
+  }
+
+  for(let column = 0; column < 3 ; column ++){
+    if(game_board[0][column] === symbol && game_board[1][column] === symbol && game_board[2][column] === symbol){
+      return true //this if loop checks if the symbol is placed 3 times in a row vertically, the it increments hence checks all the columns
+    }
+  }
+
+  //checks for diagonal win
+  if (game_board[0][0] === symbol && game_board[1][1] === symbol && game_board[2][2] === symbol){
+    return true;
+  }
+
+  if (game_board[0][2] === symbol && game_board[1][1] === symbol && game_board[2][0] === symbol){
+    return true;
+  }
+
+  return false;
+}
 
 
 //BoardCell represents a square on the gameboard
@@ -50,42 +62,43 @@ const BoardCell = () =>{
   return add_move, get_move;
 }
 
-const game_logic = () => {
-  const game_board = game_board();
-  const player1 = "Player 1";
-  const player2 = "Player 2";
+// const game_logic = () => {
+//   const game_board = game_board();
+//   const player1 = "Player 1";
+//   const player2 = "Player 2";
 
-  const players = [ //construction of players object
-    {
-      name: player1,
-      token: 1
-    },
-    {
-      name: player2,
-      token: 2
-    }
-  ];
+//   const players = [ //construction of players object
+//     {
+//       name: player1,
+//       token: 1
+//     },
+//     {
+//       name: player2,
+//       token: 2
+//     }
+//   ];
 
-  //default -> make active player player1 
-  let active_player = players[0];
+//   //default -> make active player player1 
+//   let active_player = players[0];
 
-  //switching player's turn
-  // console.log(active_player.name);
-  const switch_player_turn = () => {
-    if (active_player.token === 1){
-      active_player = players[1];
-    }
-  };
+//   //switching player's turn
+//   // console.log(active_player.name);
+//   const switch_player_turn = () => {
+//     if (active_player.token === 1){
+//       active_player = players[1];
+//     }
+//   };
 
-  const get_active_player = () => {
-    return active_player;
-  }
+//   const get_active_player = () => {
+//     return active_player;
+//   }
 
-  const new_round = () =>{
-    //add logic here to print a new round
-  }
+//   const new_round = () =>{
+//     //add logic here to print a new round
+//   }
 
 
-}
+// }
 
-GameLogic();
+console.log(game_board());
+// game_logic();
